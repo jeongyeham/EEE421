b6c64c2d9a38b597e45ec1d19a0174f8dc2224a8
jeongyeham <jeongyeham@foxmail.com>
2024-11-18 09:46:24 +0800

    Sync my latest code in Tensorflow tech

--- my_tensorflow.py
+++ my_tensorflow.py
@@ -1,42 +1,81 @@
-import keras
-import numpy as np
 import pandas as pd
-from joblib import dump, load
+import numpy as np
+import keras
 from keras import Model
-from keras.src.layers import Dense, Dropout, BatchNormalization
-from keras.src.optimizers import Adam, Adamax
+from keras.src.layers import Dense, Dropout
+from keras.src.optimizers import Adam
+from pygments.lexers import go
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from joblib import dump, load
 from matplotlib import pyplot as plt
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
 from statsmodels.api import qqplot
+
+class BuildingEnergyAnalyzerDynamic:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = self._read_data()
+
+    def _read_data(self):
+        data = pd.read_csv(self.file_path)
+        return data
+
+    def plot_each_column_as_scatter(self):
+        if self.data is not None:
+            for column in self.data.columns:
+                fig = go.Figure(data=[go.Scatter(
+                    x=self.data.index,
+                    y=self.data[column],
+                    mode='markers',
+                    name=column
+                )])
+                fig.update_layout(
+                    title=f'Scatter Plot of {column}',
+                    xaxis_title='Row Number',
+                    yaxis_title=column
+                )
+                fig.show()
+        else:
+            print("Data is not loaded. Cannot plot.")
+
+class BuildingEnergyAnalyzerStatic:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = self._read_data()
+
+    def _read_data(self):
+        data = pd.read_csv(self.file_path, index_col=0)
+        return data
+
+    def plot_columns(self):
+        if self.data is not None:
+            for column in self.data.columns:
+                plt.figure()
+                plt.scatter(range(self.data.shape[0]), self.data[column])
+                plt.title(f'Scatter Plot of {column}')
+                plt.xlabel('Row Number')
+                plt.ylabel(column)
+                plt.grid(True)
+                plt.show()
+        else:
+            print("Data is not loaded. Cannot plot.")
 
 class EnergyModel(Model):
     def __init__(self):
         super().__init__()
-
-        self.preprocessor = None
-        self.model_path = "./build/model.keras"
-        self.training_file_path = "./Training data.csv"
-        self.preprocessor_path = "./build/setting/preprocessor.pkl"
-        self.target_scaler_path = "./build/setting/target_scaler.pkl"
-
-        self.base_year = 1950
-        self.base_postcode = 53
-
-        self.feature_data = None
-        self.target_data = None
-
-        self.dense_1 = Dense(512, activation='relu')
-        self.dropout_1 = Dropout(0.3)
+        self.features = None
+        self.target = None
+        self.dense_1 = Dense(25, activation='relu')
+        self.dropout_1 = Dropout(0.2)
         self.dense_2 = Dense(512, activation='relu')
-        self.dropout_2 = Dropout(0.3)
-        self.dense_3 = Dense(256, activation='relu')
-        self.dropout_3 = Dropout(0.3)
+        self.dropout_2 = Dropout(0.2)
+        self.dense_3 = Dense(128, activation='relu')
+        self.dropout_3 = Dropout(0.2)
         self.dense_4 = Dense(1, activation='linear')
+        self.scaler = StandardScaler()
 
-        self.target_scaler = StandardScaler()
-
-    def call(self, inputs, training=False):
+    def call(self, inputs, training=None):
         x = self.dense_1(inputs)
         x = self.dropout_1(x, training=training)
         x = self.dense_2(x)
@@ -47,85 +86,44 @@ class EnergyModel(Model):
 
     def build(self, input_shape):
         super().build(input_shape)
-
-    def preprocess_training_data(self):
-        data = pd.read_csv(self.training_file_path).drop(columns=['Index'])
-        target_data = data['ENERGY_CONSUMPTION_CURRENT']
-        feature_data = data.drop(columns=['ENERGY_CONSUMPTION_CURRENT'])
-
-        feature_data['POSTCODE_DIFF'] = feature_data['POSTCODE'] - self.base_postcode
-        feature_data = feature_data.drop(columns=['POSTCODE'])
-
-        # 处理时间年份特征
-        feature_data['YEAR_DIFF'] = feature_data['CONSTRUCTION_AGE_BAND'] - self.base_year
-        feature_data = feature_data.drop(columns=['CONSTRUCTION_AGE_BAND'])
-
-        # 定义数值和分类特征
-        numeric_features = ['TOTAL_FLOOR_AREA', 'NUMBER_HABITABLE_ROOMS',
-                            'NUMBER_HEATED_ROOMS', 'YEAR_DIFF', 'POSTCODE_DIFF']
         
-        categorical_features = ['PROPERTY_TYPE', 'BUILT_FORM', 'TRANSACTION_TYPE', 'ENERGY_TARIFF', 'FLOOR_LEVEL',
-                                'GLAZED_TYPE', 'GLAZED_AREA', 'HOT_WATER_ENERGY_EFF', 'FLOOR_DESCRIPTION',
-                                'FLOOR_ENERGY_EFF', 'WINDOWS_ENERGY_EFF', 'WALLS_ENERGY_EFF', 'ROOF_DESCRIPTION',
-                                'ROOF_ENERGY_EFF', 'MAINHEAT_ENERGY_EFF', 'MAINHEATC_ENERGY_EFF',
-                                'LIGHTING_DESCRIPTION', 'LIGHTING_ENERGY_EFF', 'TENURE']
-        
-        energy_efficiency_features = ['CURRENT_ENERGY_EFFICIENCY', 'LOW_ENERGY_LIGHTING']
-        # 定义预处理器
-        self.preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', StandardScaler(), numeric_features),
-                ('energy_eff', MinMaxScaler(), energy_efficiency_features),
-                ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
-            ])
-        feature_data_scaled = self.preprocessor.fit_transform(feature_data)
-        print(feature_data_scaled.shape)
-
-        target_data_scaled = self.target_scaler.fit_transform(target_data.values.reshape(-1, 1))
-
-        # 保存标准化器和预处理器
-        dump(self.preprocessor, self.preprocessor_path)
-        dump(self.target_scaler, self.target_scaler_path)
-
-        return feature_data_scaled, target_data_scaled
+    def preprocess_training_data(self, file_path='./Training data.csv'):
+        data = pd.read_csv(file_path).drop(columns=['Index'])
+        self.target = data['ENERGY_CONSUMPTION_CURRENT']
+        self.features = data.drop(columns=['ENERGY_CONSUMPTION_CURRENT'])
+        features_scaled = self.scaler.fit_transform(self.features)
+        target_scaled = self.scaler.fit_transform(self.target.values.reshape(-1, 1))
+        return train_test_split(features_scaled, target_scaled, test_size=0.1)
 
     def energy_model_compile(self):
-        super().compile(optimizer=Adamax(learning_rate=0.001), loss='huber', metrics=['mae', keras.metrics.R2Score()])
+        super().compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mae'])
 
-    def energy_model_train(self, feature_train_data, target_train_data):
-        early_stopping = keras.callbacks.EarlyStopping(monitor='mae', patience=20, restore_best_weights=True)
-        history = self.fit(feature_train_data, target_train_data, batch_size=32, epochs=400, validation_data=,
-                           callbacks=[early_stopping])
-
-        self.energy_model_save()
+    def energy_model_train(self, feature_data, target_data):
+        early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=30, restore_best_weights=True)
+        history = super().fit(feature_data, target_data, batch_size=64, epochs=1000, validation_split=0.2,
+                                               callbacks=[early_stopping])
         return history
 
-    def energy_model_save(self, overwrite=True):
-        super().save(self.model_path, overwrite=overwrite)
+    def energy_model_save(self, model_path='./model.keras', overwrite=True):
+        super().save(model_path, overwrite=overwrite)
 
-    def energy_model_load(self):
-        return keras.models.load_model(self.model_path)
+    def energy_model_scaler_save(self, scaler_path='./scaler.pkl'):
+        dump(self.scaler, scaler_path)
 
-    def energy_model_preprocess_new_data(self, new_data):
-        new_data['POSTCODE_DIFF'] = new_data['POSTCODE'] - self.base_postcode
-        new_data = new_data.drop(columns=['POSTCODE'])
+    def energy_model_scaler_load(self, scaler_path='./scaler.pkl'):
+        self.scaler = load(scaler_path)
 
-        new_data['YEAR_DIFF'] = new_data['CONSTRUCTION_AGE_BAND'] - self.base_year
-        new_data = new_data.drop(columns=['CONSTRUCTION_AGE_BAND'])
+    def energy_model_load(self, model_path='./model.keras'):
+        return keras.models.load_model(model_path)
 
-        self.preprocessor = load(self.preprocessor_path)
-        new_data_scaled = self.preprocessor.transform(new_data)
-        return new_data_scaled
-
-    def energy_model_postprocess_predictions(self, predictions_scaled):
-        self.target_scaler = load(self.target_scaler_path)
-        predictions = self.target_scaler.inverse_transform(predictions_scaled)
-        return predictions
+    def energy_model_evaluate(self, feature_data, target_data):
+        test_loss, test_mae = super().evaluate(feature_data, target_data)
+        print(f"Test Loss: {test_loss}, Test MAE: {test_mae}")
 
     def energy_model_predict(self, new_data):
-        new_data_scaled = self.energy_model_preprocess_new_data(new_data)
-        predictions_scaled = self.predict(new_data_scaled)
-        predictions = self.energy_model_postprocess_predictions(predictions_scaled)
+        new_data_scaled = self.scaler.transform(new_data)
+        predictions_scaled = super().predict(new_data_scaled)
+        predictions = self.scaler.inverse_transform(predictions_scaled)
         return predictions
 
     def sensitivity_analysis(self, feature_data, feature_name):
@@ -140,40 +138,31 @@ class EnergyModel(Model):
             feature_data[:, i] = original_value
         return results
 
-
 def data_analyse():
     analyzer = BuildingEnergyAnalyzerDynamic('./Training data.csv')
     data = analyzer.data["TOTAL_FLOOR_AREA"]
     qqplot(data, line='45')
     plt.show()
 
-
-def model_predict_xlsx():
-    data = pd.read_excel('./published_group_data_2024.xlsx').drop(columns=['Group_number'])
-    data_feature = data.drop(columns=['ENERGY_CONSUMPTION_CURRENT']).iloc[:4]
+def model_training():
     model = EnergyModel()
-    feature_train_data, target_train_data = model.preprocess_training_data()
+    X_train, X_test, y_train, y_test = model.preprocess_training_data()
     model.energy_model_compile()
-    model.energy_model_train(feature_train_data, target_train_data)
-    predicted_data = model.energy_model_predict(data_feature)
-    print(predicted_data)
-
-
-def sensitivity_analyse(model, feature_data):
+    model.energy_model_train(X_train, y_train)
+    model.energy_model_evaluate(X_test, y_test)
     feature_names = ['POSTCODE', 'CURRENT_ENERGY_EFFICIENCY', 'PROPERTY_TYPE', 'BUILT_FORM', 'TRANSACTION_TYPE',
                      'TOTAL_FLOOR_AREA', 'ENERGY_TARIFF', 'FLOOR_LEVEL', 'GLAZED_TYPE', 'GLAZED_AREA',
                      'NUMBER_HABITABLE_ROOMS',
                      'FLOOR_ENERGY_EFF',
                      'WINDOWS_ENERGY_EFF', 'WALLS_ENERGY_EFF', 'ROOF_DESCRIPTION', 'ROOF_ENERGY_EFF',
-                     'MAINHEATC_ENERGY_EFF', 'LIGHTING_DESCRIPTION', 'LIGHTING_ENERGY_EFF',
+                     'MAINHEATC_ENERGY_EFF', 'LIGHTING_DESCRIPTION', 'LIGHTING_ENERGY_EFF', 'CONSTRUCTION_AGE_BAND',
                      'TENURE']
 
-    sensitivity_results = model.sensitivity_analysis(feature_data, feature_names)
-    print(sensitivity_results)
-
+    #sensitivity_results = model.sensitivity_analysis(X_test, feature_names)
+    #print(sensitivity_results)
 
 if __name__ == "__main__":
-    model_predict_xlsx()
+    model_training()
     # 请确保提供正确的新数据格式
     # model = EnergyModel()
     # new_data = pd.read_excel('./published_group_data_2024.xlsx')
